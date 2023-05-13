0f970deac0568d0c0640f9110933123cec32d8a3
Kipkoech <nickey968@gmail.com>
2023-05-13 09:03:42 +0300

    add data

---
+++ Config/configs.py
@@ -0,0 +1,109 @@
+'''set up the config for brats dataset, train and test folders names and
+some parameters'''
+import os
+import torch
+import monai
+from monai.metrics import DiceMetric
+from monai.utils.enums import MetricReduction
+from monai.networks.nets import SwinUNETR
+from monai.losses import DiceLoss
+from monai.inferers import sliding_window_inference
+from monai import transforms
+from monai.transforms import (
+    AsDiscrete,
+    Activations,
+)
+from functools import partial
+
+class Config:
+    root_dir = "/gdrive/MyDrive/THESIS BraTs NUST"
+    train_root_dir = "/gdrive/MyDrive/THESIS BraTs NUST/MICCAI_BraTS2020_TrainingData/MICCAI_BraTS2020_TrainingData"
+    test_root_dir  = "/gdrive/MyDrive/THESIS BraTs NUST/MICCAI_BraTS2020_ValidationData/MICCAI_BraTS2020_ValidationData"
+    path_to_train_csv = "/gdrive/MyDrive/THESIS BraTs NUST/train_data.csv"
+    seed = 50
+    survival_info_csv_train = "/gdrive/MyDrive/THESIS BraTs NUST/MICCAI_BraTS2020_TrainingData/MICCAI_BraTS2020_TrainingData/survival_info.csv"
+    name_mapping_csv_train = "/gdrive/MyDrive/THESIS BraTs NUST/MICCAI_BraTS2020_TrainingData/MICCAI_BraTS2020_TrainingData/name_mapping.csv"
+    survival_info_csv_test = "/gdrive/MyDrive/THESIS BraTs NUST/MICCAI_BraTS2020_ValidationData/MICCAI_BraTS2020_ValidationData/survival_evaluation.csv"
+    name_mapping_csv_test = "/gdrive/MyDrive/THESIS BraTs NUST/MICCAI_BraTS2020_ValidationData/MICCAI_BraTS2020_ValidationData/name_mapping_validation_data.csv"
+    validation_csv = "/gdrive/MyDrive/THESIS BraTs NUST/MICCAI_BraTSXX_TestData/Testset/test_set.csv"
+    a_test_patient = "/gdrive/MyDrive/THESIS BraTs NUST/MICCAI_BraTSXX_TestData/Testset/BraTS19_TCIA08_167_1"
+    LGG18 = "/gdrive/MyDrive/Brats18/LGG"
+    HGG18 = "/gdrive/MyDrive/Brats18/HGG"
+    data_dir = "/gdrive/MyDrive/THESIS BraTs NUST"
+    brats18_val_data = "/gdrive/MyDrive/THESIS BraTs NUST/MICCAI_BraTSXX_TestData/Testset"
+    brats19_test_data = "/gdrive/MyDrive/THESIS BraTs NUST/MICCAI_BraTS19_Test/MICCAI_BraTS19_Test"
+    brats19_test_survival_csv = "/gdrive/MyDrive/THESIS BraTs NUST/MICCAI_BraTS19_Test/MICCAI_BraTS19_Test/test_set.csv"
+    
+    class Testset:
+        Brat19_HGG_path = "/gdrive/MyDrive/Medical Dataset/MICCAI_BraTS_2019_Data_Training/HGG"
+        Brats19_LGG_path = "/gdrive/MyDrive/Medical Dataset/MICCAI_BraTS_2019_Data_Training/LGG"
+        Brats19_TestData = "/gdrive/MyDrive/Medical Dataset/MICCAI_BraTS_2019_Data_Training/Testset"
+        size = 41
+        HGG_files = 21
+        LGG_files = 20
+        val_dir_path = "/gdrive/MyDrive/THESIS BraTs NUST/MICCAI_BraTSXX_TestData/Testset"
+        # '/gdrive/MyDrive/THESIS BraTs NUST/MICCAI_BraTSXX_TestData/Testset'
+    class newGlobalConfigs:
+        root_dir = "E:/Brats21 Data/training"
+        train_root_dir = root_dir + "/RSNA_ASNR_MICCAI_BraTS2021_TrainingData_16July2021"+ "/RSNA_ASNR_MICCAI_BraTS2021_TrainingData_16July2021"
+        test_root_dir = "E:/Brats21 Data/validation/RSNA_ASNR_MICCAI_BraTS2021_ValidationData/RSNA_ASNR_MICCAI_BraTS2021_ValidationData"
+        path_to_csv = root_dir + "/BraTS21-17_Mapping.csv"
+        pretrained_model = ""
+        survival_info_df = ""
+        a_test_patient = "BraTS2021_00000" # change it to other patients, if needed
+        full_patient_path = train_root_dir +"/" + a_test_patient
+        name_mapping_df = path_to_csv
+        seed = 50
+        json_file = root_dir + "/brats21_folds.json"
+        
+        class OtherPC:
+            root_dir = "/content"
+            train_root_dir = root_dir + "/RSNA_ASNR_MICCAI_BraTS2021_TrainingData_16July2021"
+            path_to_csv = root_dir + "/BraTS21-17_Mapping.csv"
+            a_test_patient = "BraTS2021_00000" # change it to other patients, if needed
+            full_patient_path = train_root_dir +"/" + a_test_patient
+            name_mapping_df = path_to_csv
+            seed = 50
+            json_file = root_dir + "/brats21_folds.json"
+
+        class swinUNetCongis:
+            roi = (128, 128, 128)
+            fold = 1
+            max_epochs = 100
+            infer_overlap = 0.5
+            val_every = 2
+            class training_cofigs:
+                roi = (128, 128, 128)
+                num_workers = 2
+                batch_size = 1
+                sw_batch_size = 1
+                infer_overlap = 0.6
+                dice_loss = DiceLoss(to_onehot_y=False, sigmoid=True)
+                post_simgoid = Activations(sigmoid= True)
+                post_pred = AsDiscrete(argmax= False, threshold= 0.5)
+                dice_acc = DiceMetric(include_background=True, reduction=MetricReduction.MEAN_BATCH, 
+                                      get_not_nans=True)
+                device = "cuda" if torch.cuda.is_available() else "cpu"
+                model = SwinUNETR(
+                                    img_size=roi,
+                                    in_channels=4,
+                                    out_channels=3,
+                                    feature_size=48,
+                                    drop_rate=0.0,
+                                    attn_drop_rate=0.0,
+                                    dropout_path_rate=0.0,
+                                    use_checkpoint=True,
+                                ).to(device)
+                
+                model_inferer = partial(
+                                        sliding_window_inference,
+                                        roi_size=[roi[0], roi[1], roi[2]],
+                                        sw_batch_size=sw_batch_size,
+                                        predictor=model,
+                                        overlap=infer_overlap)
+                learning_rate = 1e-4
+                weight_decay = 1e-5
+                max_epochs = 100
+                optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, 
+                                              weight_decay=weight_decay)
+                scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=max_epochs)
---
+++ Loaders/dataset.py
@@ -0,0 +1,164 @@
+'''BraTS 2021 Data loader class'''
+import numpy as np
+import torch
+from torch.utils.data import DataLoader, Dataset
+import pandas as pd
+import nibabel as nib
+import json
+import csv
+import os
+from skimage.transform import resize
+from pathlib import Path
+import sys
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0].parents[0]  #project root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+
+from config.configs import Config
+from utils.preprocess import data_transforms
+from utils.preprocess import insert_cases_paths_to_df
+
+class BraTSDataset(Dataset):
+    """Load BraTS 2021 dataset from the disk for training and validation and testing
+    as per the requirment
+    
+    Parameters
+    ----------
+    df: pd.DataFrame
+    phase: str
+    is_resize: bool
+    """
+    def __init__(self, df: pd.DataFrame, phase: str = "val", is_resize: bool = False):
+        self.df = df
+        self.phase = phase
+        self.is_resize = is_resize
+        self.augmentations = data_transforms(phase= phase)
+        self.data_types = ['_flair.nii.gz', '_t1.nii.gz', '_t1ce.nii.gz', '_t2.nii.gz']
+
+    def __len__(self):
+        return self.df.shape[0]
+    
+    def __getitem__(self, index):
+        case = self.df.loc[index, "BraTS2021"]
+        root_path = self.df.loc[self.df['BraTS2021'] == case]['path'].values[0]
+
+        modalities = [] 
+        for data_type in self.data_types:
+            img_path = root_path + "/" + case + data_type
+            img = self.load_img(img_path)
+            
+            if self.is_resize:
+                img = self.resize(img)
+    
+            img = self.normalize(img)
+            modalities.append(img)
+        img = np.stack(modalities)
+        img = np.moveaxis(img, (0, 1, 2, 3), (0, 3, 2, 1))
+        
+        if self.phase != "test":
+            mask_path =  os.path.join(root_path, case + "_seg.nii.gz")
+            mask = self.load_img(mask_path)
+            
+            if self.is_resize:
+                mask = self.resize(mask)
+                mask = np.clip(mask.astype(np.uint8), 0, 1).astype(np.float32)
+                mask = np.clip(mask, 0, 1)
+            mask = self.preprocess_mask_labels(mask)
+            data = {"image": img.astype(np.float32),
+                    "label": mask.astype(np.float32)}
+            
+            augmented = self.augmentations(data)
+            
+            img = augmented['image']
+            mask = augmented['label']
+    
+        
+            return {
+                "image": img,
+                "label": mask,
+            }
+        
+        return {
+            "image": img,
+        }
+
+
+
+
+
+    def load_img(self, file_path):
+        data = nib.load(file_path)
+        data = np.asarray(data.dataobj)
+        return data
+    
+    def normalize(self, data: np.ndarray):
+        data_min = np.min(data)
+        return (data - data_min) / (np.max(data) - data_min)
+    
+    def resize(self, data: np.ndarray):
+        data = resize(data, (78, 120, 120), preserve_range=True)
+        return data
+    
+    def preprocess_mask_labels(self, mask: np.ndarray):
+
+        mask_WT = mask.copy()
+        mask_WT[mask_WT == 1] = 1
+        mask_WT[mask_WT == 2] = 1
+        mask_WT[mask_WT == 4] = 1
+
+        mask_TC = mask.copy()
+        mask_TC[mask_TC == 1] = 1
+        mask_TC[mask_TC == 2] = 0
+        mask_TC[mask_TC == 4] = 1
+
+        mask_ET = mask.copy()
+        mask_ET[mask_ET == 1] = 0
+        mask_ET[mask_ET == 2] = 0
+        mask_ET[mask_ET == 4] = 1
+
+        mask = np.stack([mask_WT, mask_TC, mask_ET])
+        mask = np.moveaxis(mask, (0, 1, 2, 3), (0, 3, 2, 1))
+
+        return mask
+
+
+def get_dataloader(
+    dataset: torch.utils.data.Dataset,
+    path_to_csv: str,
+    phase: str,
+    batch_size: int = 1,
+    num_workers: int = 2,
+    json_file: str = None,
+    fold: int = 0,
+    train_dir = Config.newGlobalConfigs.train_root_dir,
+    test_dir = Config.newGlobalConfigs.test_root_dir):
+
+    '''Returns: dataloader for the model training'''
+    df = insert_cases_paths_to_df(path_to_csv, train_dir = train_dir, 
+                                  test_dir= test_dir, json_file=json_file, fold=fold)
+    
+    train_df = df.loc[df['phase'] == 'train'].reset_index(drop=True)
+    val_df = df.loc[df['phase'] == 'val'].reset_index(drop=True)
+
+    df = train_df if phase == "train" else val_df
+    dataset = dataset(df, phase)
+    print(f'Total examples in the dataset: {len(df)}')
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        pin_memory=True,
+        shuffle=True,   
+    )
+    return dataloader
+
+if __name__ == '__main__':
+    json_file = Config.newGlobalConfigs.json_file
+    data = get_dataloader(BraTSDataset, Config.newGlobalConfigs.path_to_csv, 'train', 2, 2, json_file=json_file)
+    batch = next(iter(data))
+    image, label = batch["image"], batch['label']
+    print(image.shape, label.shape)
+    
